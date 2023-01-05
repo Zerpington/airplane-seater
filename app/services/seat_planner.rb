ead5cf6953f7561bf2db5d2f44506ee402f10cc0
Zacky Jat <zackyj@pop-os.localdomain>
2023-01-05 14:27:25 +0800

    Add simple verification

--- app/services/seat_planner.rb
+++ app/services/seat_planner.rb
@@ -1,7 +1,42 @@
 class SeatPlanner
     def initialize
         @seats = [[3,2], [4,3], [2,3], [3,4]]
+        # possible test cases for later
+        # @seats = [[3,2], [4,3], [2,3], [0, 1]]
+        # @seats = [[3,2], [4,3], [2,3], [2, nil]]
+        # @seats = [[3,2], [4,3], [2,3], [2]]
+        # @seats = [[3,2], [4,3], [2,3], [1, "a"]]
         @passengers = 30
+    end
+
+    def seat_verification
+        total_seats = 0
+        @seats.map { |seat|
+            unless seat.length == 2
+                # render json: {}, status: :unprocessable_entity
+                print "Error: Seat Format - Array length not followed"
+                return false
+            end
+            unless seat.exclude? 0 || nil
+                # render json: {}, status: :unprocessable_entity
+                print "Error: Seat Format - Includes 0/nil"
+                return false
+            end
+            unless seat.all? {|i| i.is_a?(Integer) }
+                # render json: {}, status: :unprocessable_entity
+                print "Error: Seat Format - Non-Integer"
+                return false
+            end
+            total_seats += seat.inject(:*)
+        }
+        unless total_seats > @passengers
+            print "Error: Too many Passengers"  
+            return false
+            # flash[:alert] = "Seating Error"
+        else
+            print "Seats Verified"
+            return true
+        end
     end
 
     def block_maker (seat, i)
@@ -37,8 +72,11 @@ class SeatPlanner
                     row.insert(1,["M"])
                 }
             end
+
             block = []
-            seat[1].times{block.push(row)}
+            rows = seat[1]
+            rows.times{block.push(row)}
+            
             return block
         else
             case seat[0]
